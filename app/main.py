4917e03c225ba8f52159a286ced84a06d8585f20
Apoorva Arbooj <apoorvaarbooj9@gmail.com>
2025-03-14 04:43:11 -0500

    Type and execs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
 import sys
+import os
+PATH_SEP = os.pathsep
+PATH = os.environ["PATH"]
 
+BUILTINS = {"exit": "builtin", "type": "builtin", "echo": "builtin", "pwd": "builtin"}
+
+def parse_path(path):
+    path = path.split(PATH_SEP)
+    path = [current_path for current_path in path if current_path]
+    for current_path in path:
+        if os.path.exists(current_path):
+            files = (
+                file
+                for file in os.listdir(current_path)
+                if os.path.isfile(os.path.join(current_path, file))
+            )
+        for file in files:
+            if file not in BUILTINS:
+                BUILTINS[file] = os.path.join(current_path, file)
 def main():
+    parse_path(PATH)
     while True:
         sys.stdout.write("$ ")
         args = input().split(" ")
@@ -12,6 +31,17 @@ def main():
                 return 0
             elif command == "echo":
                 print(*params)
+            elif command == "type":
+                arg_command = params[0]
+                if arg_command in BUILTINS:
+                    if BUILTINS[arg_command] == "builtin":
+                        print(f"{arg_command} is a shell builtin")
+                    else:
+                        print(f"{arg_command} is {BUILTINS[arg_command]}")
+                else:
+                    print(f"{arg_command}: not found")
+            elif command in BUILTINS:
+                os.system(f"{command} {' '.join(params)}")
             else:
                 print(f"{command}: command not found")
 if __name__ == "__main__":
