cc99fa448163f2ebaf1195b322ca7c684b75bc44
Apoorva Arbooj <apoorvaarbooj9@gmail.com>
2025-03-14 04:38:14 -0500

    Handle invalid commands

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import sys
 
-
 def main():
-    sys.stdout.write("$ ")
-
-    # Wait for user input
-    input()
-
-
+    while True:
+        sys.stdout.write("$ ")
+        args = input().split(" ")
+        if len(args) == 0:
+            continue
+        else:
+            command, *params = args
+            print(f"{command}: command not found")
 if __name__ == "__main__":
     main()
